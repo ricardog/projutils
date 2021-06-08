8800d54bd5a09a281c094b8fb0a4cad54c9494f2
Ricardo E. Gonzalez <ricardog@ricardog.com>
2021-06-08 16:28:51 +0100

    Bump version
    Removal of names requires a new version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="projutils",
-    version="0.1",
+    version="0.2",
     author="Ricardo E. Gonzalez",
     author_email="ricardog@itinerisinc.com",
     description="Utilities for generating and visualizing raster maps",
