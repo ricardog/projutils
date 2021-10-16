1bdbfbad0bd85b3c8118f7261d0331ea61b44561
Ricardo E. Gonzalez <ricardog@ricardog.com>
2021-10-16 14:49:46 -0700

    Add a couple more tests for LUI class
    Verify the sum of minimal, light, plus intense is (approximately) equal
    to the data passed in.

--- tests/lui_test.py
+++ tests/lui_test.py
@@ -3,16 +3,66 @@ import pandas as pd
 
 from projutils.lui import LUI
 
-def test_lui_cropland():
+def test_lui_base():
     hpd = np.arange(0, 11, 0.1, dtype="float32")
     shape = hpd.shape
     unsub = np.full(shape, 5, dtype="float32")
     crop = np.random.rand(*shape).astype("float32")
-    df = pd.DataFrame({"hpd": hpd,
-                       "unSub": unsub,
-                       "crop": crop})
     lui = LUI("crop", "intense", "cropland")
     _ = lui.eval({"hpd": hpd,
                   "unSub": unsub,
                   "crop": crop})
+    return
+
+
+def test_lui_minimal():
+    hpd = np.arange(0, 11, 0.1, dtype="float32")
+    shape = hpd.shape
+    unsub = np.full(shape, 5, dtype="float32")
+    crop = np.random.rand(*shape).astype("float32")
+    #crop = np.linspace(0, 1, shape[0])
+    #crop = np.full(shape, 0.1, dtype="float32")
+    intense = LUI("crop", "intense", "cropland")
+    light = LUI("crop", "light", "cropland")
+    minimal = LUI("crop", "minimal", "cropland")
+
+    crop_int = intense.eval({"hpd": hpd,
+                             "unSub": unsub,
+                             "crop": crop})
+    crop_light = light.eval({"hpd": hpd,
+                             "unSub": unsub,
+                             "crop": crop,
+                             "crop_intense": crop_int})
+    
+    crop_min = minimal.eval({"hpd": hpd,
+                             "unSub": unsub,
+                             "crop": crop,
+                             "crop_intense": crop_int,
+                             "crop_light": crop_light})
+    assert np.all(crop_min >= -1e-6)
+    return
+
+
+def test_lui_sum():
+    hpd = np.arange(0, 11, 0.1, dtype="float32")
+    shape = hpd.shape
+    unsub = np.full(shape, 5, dtype="float32")
+    crop = np.random.rand(*shape).astype("float32")
+    intense = LUI("crop", "intense", "cropland")
+    light = LUI("crop", "light", "cropland")
+    minimal = LUI("crop", "minimal", "cropland")
+
+    crop_int = intense.eval({"hpd": hpd,
+                             "unSub": unsub,
+                             "crop": crop})
+    crop_light = light.eval({"hpd": hpd,
+                             "unSub": unsub,
+                             "crop": crop,
+                             "crop_intense": crop_int})
+    crop_min = minimal.eval({"hpd": hpd,
+                             "unSub": unsub,
+                             "crop": crop,
+                             "crop_intense": crop_int,
+                             "crop_light": crop_light})
+    assert np.allclose(crop, crop_int + crop_light + crop_min)
     return
